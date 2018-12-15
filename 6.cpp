213268b367230144721becfada74c967abb7d2d1
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:41:35 +0700

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+float V,S,T;
+cout<<"MENGHITUNG JARAK TEMPUH"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"masukan nilai kecepatan:";
+cin>>V;
+cout<<"masukan nilai waktu:";
+cin>>T;
+S=V*T;
+cout<<"Maka jarak tempuh adalah "<<S;
+return 0;
+}
